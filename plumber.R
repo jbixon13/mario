922a7dd38418f6c62177332571af0436d08affe4
jbixon13 <jbixon13@gmail.com>
2020-02-18 22:28:57 -0500

    remove example endpoints

--- plumber.R
+++ plumber.R
@@ -5,11 +5,5 @@ root <- plumber$new()
 hello_api <- plumber$new("hello/world.R")
 root$mount("/hello", hello_api)
 
-diamond_api <- plumber$new("diamond/plot.R")
-root$mount("/diamond", diamond_api)
-
-mpg_api <- plumber$new("mpg/plot.R")
-root$mount("/mpg", mpg_api)
-
 mta_article_api <- plumber$new("MTA_article/plot.R")
 root$mount("/MTA_article", mta_article_api)
